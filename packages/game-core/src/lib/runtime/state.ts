47018df45925f51cfba0bab48764c50a11c9f3cb
Leah Masson <elvynia@gmail.com>
2022-08-28 01:12:50 +0200

    Fixed RuntimeState type requiring both constant and observable features.

--- packages/game-core/src/lib/runtime/state.ts
+++ packages/game-core/src/lib/runtime/state.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import { FeatureState } from '../feature/state';
 
 export type RuntimeState<S extends FeatureState> = {
-    [K in keyof S]: S[K];
+    [K in keyof S]?: S[K];
 } & {
-    [G in keyof S as `${G & string}$`]: Observable<S[G]>;
+    [G in keyof S as `${G & string}$`]?: Observable<S[G]>;
 }
