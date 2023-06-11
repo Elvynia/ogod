ecbf9e27983f4a8ed1096cf6348a701cbf9d2a69
Leah Masson <elvynia@gmail.com>
2023-06-11 22:18:03 +0200

    Enhanced tweenObject observable by only publishing on complete and better typing.

--- packages/core/src/lib/observable/tween-object.ts
+++ packages/core/src/lib/observable/tween-object.ts
@@ -1,23 +1,23 @@
 import { Observable, map } from "rxjs";
-import { UpdateState } from "../update";
 import { EaseFn, animate } from "../operator/animate";
+import { UpdateState } from "../update";
 import { fromTo } from "./tween";
 
 export interface TweenValueAbstract<T> {
-    value?: number;
+    value?: T;
     valueFn?: (d: number) => T;
     easeFn?: EaseFn;
 }
 
-export interface TweenValue extends TweenValueAbstract<number> {
-    value: number;
+export interface TweenValue<T> extends TweenValueAbstract<T> {
+    value: T;
 }
 
 export interface TweenValueFn<T> extends TweenValueAbstract<T> {
     valueFn: (d: number) => T;
 }
 
-export type TweenTarget<T> = TweenValue | TweenValueFn<T>;
+export type TweenTarget<T> = TweenValue<T> | TweenValueFn<T>;
 
 export type TweenTargets<T extends object> = {
     [K in keyof Partial<T>]: TweenTarget<T[K]>;
@@ -30,22 +30,27 @@ export function tweenObject<T extends object>(params: {
     easeFn?: (d: number) => number,
     update$: Observable<UpdateState>
 }): Observable<T> {
-    const target = {} as any;
-    const origin = {} as any;
-    const eases = {} as any;
+    const target = {} as Record<keyof T, (d: number) => T[Extract<keyof T, string>]>;
+    const origin = {} as Record<keyof T, number>;
+    const eases = {} as Record<keyof T, EaseFn>;
     for (const k in params.target) {
-        origin[k] = params.source[k as keyof T];
-        eases[k] = params.target[k].easeFn || params.easeFn;
+        origin[k] = params.source[k] as number;
+        eases[k] = (params.target[k].easeFn || params.easeFn)!;
         target[k] = params.target[k].valueFn
-            || ((d: number) => fromTo(eases[k](d), origin[k], params.target[k].value!))
+            || ((d: number) => fromTo(eases[k](d), origin[k], params.target[k].value as number) as T[typeof k]);
     }
-    return params.update$.pipe(
-        animate(params.duration),
-        map((d) => {
-            for (const k in target) {
-                params.source[k as keyof T] = target[k](d);
-            }
-            return params.source;
-        })
-    );
+    return new Observable((subscriber) => {
+        const sub = params.update$.pipe(
+            animate(params.duration),
+            map((d) => {
+                for (const k in target) {
+                    params.source[k] = target[k](d);
+                }
+                return params.source;
+            })
+        ).subscribe({
+            complete: () => subscriber.complete()
+        });
+        return () => sub.unsubscribe();
+    });
 }
