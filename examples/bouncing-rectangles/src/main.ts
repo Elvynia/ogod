5819a97d939d73c11db6748af52f29d46fd57e12
Leah Masson <elvynia@gmail.com>
2023-06-17 15:57:06 +0200

    Adapted bouncing rectangles dom with modern helpers el function to remove some boilerplate.

--- examples/bouncing-rectangles/src/main.ts
+++ examples/bouncing-rectangles/src/main.ts
@@ -1,3 +1,4 @@
+import { el } from '@modern-helpers/el';
 import { makeMessage, makeTargetActions } from '@ogod/core';
 import { makeDriverWorker } from '@ogod/driver-worker';
 import { run } from '@ogod/run';
@@ -7,60 +8,54 @@ import { AppReflectState, AppSources } from './app/state';
 function main(sources: AppSources) {
     let paused = false;
     const playerColor = '#ff33ff';
-    const app = document.querySelector('#app');
-    const wrapper = document.createElement('div');
-    wrapper.className = 'wrapper';
-    const canvas = document.createElement('canvas');
-    canvas.width = 800;
-    canvas.height = 600;
-    // Canvas focusable only if tabindex.
-    canvas.setAttribute('tabindex', '0');
-    const paused$ = fromEvent(canvas, 'focus').pipe(
-        switchMap(() => fromEvent(canvas, 'keydown').pipe(
-            takeUntil(fromEvent(canvas, 'blur')),
-            filter((e: KeyboardEvent) => e.code === 'Space'),
-            map(() => paused = !paused)
-        )),
-        startWith(paused),
-        map((value) => makeMessage({ key: 'paused', value }))
-    );
-    const playerColorInput = document.createElement('input');
-    playerColorInput.id = 'playerColor';
-    playerColorInput.value = playerColor;
-    playerColorInput.setAttribute('length', '7');
-    const playerColor$ = fromEvent(playerColorInput, 'input').pipe(
-        map((e: Event) => (e.target as any).value),
-        filter((value) => value && value.length === 7),
-        map((value) => makeMessage({ key: 'playerColor', value }))
-    );
-    const ui = document.createElement('div');
-    ui.className = 'ui';
-    const field = document.createElement('div');
-    const label = document.createElement('label');
-    label.textContent = 'Player color';
-    label.setAttribute('for', 'playerColor');
-    field.appendChild(label)
-    field.appendChild(playerColorInput);
-    ui.appendChild(field);
-    const fps = document.createElement('div');
-    const fpsLabel = document.createElement('span');
-    fpsLabel.textContent = 'FPS: ';
-    const fpsValue = document.createElement('span');
-    fpsValue.textContent = 'N/A';
-    fps.appendChild(fpsLabel);
-    fps.appendChild(fpsValue);
-    ui.appendChild(fps);
-    wrapper.appendChild(canvas);
-    app.appendChild(wrapper);
-    app.appendChild(ui);
+    const playerColorId = 'playerColor';
+    const canvas = el('canvas', {
+        attributes: [
+            ['width', '800'],
+            ['height', '600'],
+            ['tabindex', '0']
+        ]
+    }) as HTMLCanvasElement;
+    const wrapper = el('div', { className: 'wrapper' }, [canvas]);
+    const playerColorInput = el('input', {
+        attributes: [
+            ['id', playerColorId],
+            ['value', playerColor],
+            ['maxlength', '7']
+        ]
+    }) as HTMLInputElement;
+    const fpsEl = el('span', undefined, ['N/A']);
+    const countEl = el('span', undefined, ['N/A']);
+    const boxCountEl = el('span', undefined, ['N/A']);
+    const ui = el('div', { className: 'ui' }, [
+        el('div', undefined, [
+            el('label', { attributes: [['for', playerColorId]] }, ['Player color']),
+            playerColorInput
+        ]),
+        el('div', undefined, [
+            el('span', undefined, ['FPS: ']),
+            fpsEl
+        ]),
+        el('div', undefined, [
+            el('span', undefined, ['Object count: ']),
+            countEl
+        ]),
+        el('div', undefined, [
+            el('span', undefined, ['Box2d object count: ']),
+            boxCountEl
+        ])
+    ]);
+    const app = el('div', undefined, [wrapper, ui]);
+    document.body.appendChild(app);
     const rects: Array<HTMLElement> = [];
     sources.Worker.input$.subscribe((state) => {
-        fpsValue.textContent = state.fps.toString();
+        fpsEl.textContent = state.fps.toString();
+        countEl.textContent = state.objects.length.toString();
+        boxCountEl.textContent = state.box2dCount.toString();
         for (let i = 0; i < state.objects.length; ++i) {
             const obj = state.objects[i];
             if (!rects[i]) {
-                rects[i] = document.createElement('div');
-                rects[i].className = 'rect';
+                rects[i] = el('div', { className: 'rect' });
                 wrapper.appendChild(rects[i]);
             }
             rects[i].style.color = obj.colorLight ? 'black' : 'white';
@@ -82,8 +77,20 @@ function main(sources: AppSources) {
             sources.Worker.initialized$.pipe(
                 map(() => makeMessage({ key: 'playerColor', value: playerColor }))
             ),
-            playerColor$,
-            paused$,
+            fromEvent(playerColorInput, 'input').pipe(
+                map(() => playerColorInput.value),
+                filter((value) => value && value.length === 7),
+                map((value) => makeMessage({ key: 'playerColor', value }))
+            ),
+            fromEvent(canvas, 'focus').pipe(
+                switchMap(() => fromEvent(canvas, 'keydown').pipe(
+                    takeUntil(fromEvent(canvas, 'blur')),
+                    filter((e: KeyboardEvent) => e.code === 'Space'),
+                    map(() => paused = !paused)
+                )),
+                startWith(paused),
+                map((value) => makeMessage({ key: 'paused', value }))
+            ),
             fromEvent(canvas, 'mousedown').pipe(
                 switchMap((e) => paused ? EMPTY : timer(200).pipe(
                     switchMap(() => interval(100).pipe(
